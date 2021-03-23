86655e2e5c5f383a73a77eb7be6da0ed14e106b3
Ian F <ian.ferg36@gmail.com>
2021-03-22 19:32:15 -0600

    set up transition from start page to question page and created template

---
+++ assets/script.js
@@ -0,0 +1,57 @@
+
+var startButton = document.getElementById('start-button');
+var startPage = document.getElementById('start-page')
+var questionScreen = document.getElementById('question-screen');
+
+startButton.addEventListener("click", startGame)
+
+function startGame(){
+    startPage.classList.add('hidden')
+    questionScreen.classList.remove('hidden')
+
+   
+
+
+}
+
+// Quiz Objects
+var q1 ={
+    question:" ",
+    answers : [ ],
+    solution: answers[ ],
+
+
+}
+
+var q2 ={
+    question:" ",
+    answers : [ ],
+    solution: answers[ ],
+
+
+}
+
+var q3 ={
+    question:" ",
+    answers : [ ],
+    solution: answers[ ],
+
+
+}
+
+
+var q4 ={
+    question:" ",
+    answers : [ ],
+    solution: answers[ ],
+
+
+}
+
+var q5 ={
+    question:" ",
+    answers : [ ],
+    solution: answers[ ],
+
+
+}
