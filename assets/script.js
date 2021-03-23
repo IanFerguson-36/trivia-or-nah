819e818fef6c6adfe5bdff4e11d20d8ff16ab7ee
Ian F <ian.ferg36@gmail.com>
2021-03-23 14:23:18 -0600

    added display for questions and answers

--- assets/script.js
+++ assets/script.js
@@ -1,60 +1,54 @@
-
-var startButton = document.getElementById('start-button');
-var startPage = document.getElementById('start-page')
-var questionScreen = document.getElementById('question-screen');
-var currentQIndex=0;
-var questions= [{
-    question:"Does this work? ",
-    answers : ["yes","no","absolutely not" ],
+var startButton = document.getElementById("start-button");
+var startPage = document.getElementById("start-page");
+var questionScreen = document.getElementById("question-screen");
+var currentQIndex = 0;
+var questions = [
+  {
+    question: "Does this work? ",
+    answers: ["yes", "no", "absolutely not", "sure"],
     solution: "yes",
-
-},
-{
-    question:"Does this work? ",
-    answers : ["yes","no","absolutely not" ],
+  },
+  {
+    question: "Does this work? ",
+    answers: ["yes", "no", "absolutely not", "sure"],
     solution: "yes",
-
-},
-{
-    question:"Does this work? ",
-    answers : ["yes","no","absolutely not" ],
+  },
+  {
+    question: "Does this work? ",
+    answers: ["yes", "no", "absolutely not", "sure"],
     solution: "yes",
-
-},
-{
-    question:"Does this work? ",
-    answers : ["yes","no","absolutely not" ],
+  },
+  {
+    question: "Does this work? ",
+    answers: ["yes", "no", "absolutely not", "sure"],
     solution: "yes",
-
-},
-{
-    question:"Does this work? ",
-    answers : ["yes","no","absolutely not" ],
+  },
+  {
+    question: "Does this work? ",
+    answers: ["yes", "no", "absolutely not", "sure"],
     solution: "yes",
-
-},
-
-
+  },
 ];
-var qAndA=document.getElementById("q&a-container");
 
+//DOM ELEMENTS
+var questionText = document.getElementById("question-text");
+var choicesDiv = document.getElementById("choices");
 
+startButton.addEventListener("click", startGame);
 
-
-startButton.addEventListener("click", startGame)
-
-function startGame(){
-    startPage.classList.add('hidden')
-    questionScreen.classList.remove('hidden')
-    disiplayQuestion();
-    
-
-
+function startGame() {
+  startPage.classList.add("hidden");
+  questionScreen.classList.remove("hidden");
+  disiplayQuestion();
 }
-function disiplayQuestion(){
-    questions[currentQIndex];
+function disiplayQuestion() {
+  questionText.textContent = questions[currentQIndex].question;
+  //   console.log(questions[currentQIndex]);.
 
-
+  var thisAnswers = questions[currentQIndex].answers;
+  for (var i = 0; i < thisAnswers.length; i++) {
+    var button = document.createElement("button");
+    button.textContent = thisAnswers[i];
+    choicesDiv.appendChild(button);
+  }
 }
-
-
